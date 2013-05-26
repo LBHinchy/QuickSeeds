a0c0ccc07c483921337617aa00eb2810bd71dca1
Zach Hinchy <zach@hinchy.us>
2013-05-25 22:18:01 -0500

    v0.3.0 update. added support for carrots and potatoes, the ability to toggle each crop on an individual basis, and support for creative mode, as well as better default config handling.

--- src/us/hinchy/QuickSeeds/QuickSeedsBlockListener.java
+++ src/us/hinchy/QuickSeeds/QuickSeedsBlockListener.java
@@ -2,6 +2,7 @@ package us.hinchy.QuickSeeds;
 
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,6 +11,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class QuickSeedsBlockListener implements Listener {
+	
+	public QuickSeedsPlugin plugin;
+	 
+	public QuickSeedsBlockListener(QuickSeedsPlugin instance) {
+	    plugin = instance;
+	}
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
@@ -17,8 +24,31 @@ public class QuickSeedsBlockListener implements Listener {
         if (!(player.hasPermission("quickseeds.plant"))) return;
 
         Block block = event.getBlock();
-        // If player planted seeds and has more seeds
-        if (block.getType() == Material.CROPS && player.getInventory().contains(Material.SEEDS,8)) {
+        
+        // is it a crop? what kind of crop is it?
+        Material cropType = block.getType();
+        Material seedType;
+        if (cropType == Material.CROPS) seedType = Material.SEEDS;
+        else if (cropType == Material.CARROT) seedType = Material.CARROT_ITEM;
+        else if (cropType == Material.POTATO) seedType = Material.POTATO_ITEM;
+        else return;
+        
+        // is the player allowed to use quickseeds with this crop?
+        if ((plugin.getConfig().getBoolean("crops.wheat") == false) && (cropType == Material.CROPS)) {
+        	if (plugin.getConfig().getBoolean("debug")) QuickSeedsPlugin.log.info("Can't QS " + cropType.toString() + "!");
+        	return;
+        }
+        if ((plugin.getConfig().getBoolean("crops.carrot") == false) && (cropType == Material.CARROT)) {
+        	if (plugin.getConfig().getBoolean("debug")) QuickSeedsPlugin.log.info("Can't QS " + cropType.toString() + "!");
+        	return;
+        }
+        if ((plugin.getConfig().getBoolean("crops.potato") == false) && (cropType == Material.POTATO)) {
+        	if (plugin.getConfig().getBoolean("debug")) QuickSeedsPlugin.log.info("Can't QS " + cropType.toString() + "!");
+        	return;
+        }
+        
+        // If player planted an applicable crop and has more to plant        
+        if (player.getInventory().contains(seedType,8) || (player.getGameMode() == GameMode.CREATIVE)) {
             // stores how many seeds are used and thus need to be removed from the inventory
             int seeds = 1;
             // Loop around the 8 bordering blocks
@@ -28,15 +58,15 @@ public class QuickSeedsBlockListener implements Listener {
                         Block neighbour = block.getRelative(x, 0, z);
                         Block neighbourBelow = neighbour.getRelative(BlockFace.DOWN);
                         if (neighbourBelow.getType() == Material.SOIL && neighbour.getType() == Material.AIR) {
-                            if (player.getInventory().contains(Material.SEEDS)) {
-                                neighbour.setType(Material.CROPS);
+                            if (player.getInventory().contains(seedType)) {
+                                neighbour.setType(cropType);
                                 seeds++;
                             }
                         }
                     }
                 }
             }
-            removeItems(player, Material.SEEDS, seeds);
+            if (player.getGameMode() != GameMode.CREATIVE) removeItems(player, seedType, seeds);
         }
     }
 
--- src/us/hinchy/QuickSeeds/QuickSeedsPlugin.java
+++ src/us/hinchy/QuickSeeds/QuickSeedsPlugin.java
@@ -1,8 +1,11 @@
 package us.hinchy.QuickSeeds;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class QuickSeedsPlugin extends JavaPlugin {
@@ -11,31 +14,39 @@ public class QuickSeedsPlugin extends JavaPlugin {
 
     public void onDisable() {
         // Output to console that plugin is disabled
-        PluginDescriptionFile pdfFile = this.getDescription();
-        System.out.println(pdfFile.getName() + " version " + pdfFile.getVersion() + " disabled!");
+        System.out.println(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " disabled!");
     }
 
     public void onEnable() {
     	// Enable log
     	log = new QuickSeedsLogger(this);
     	
-        // Load config
-		this.getConfig();
-		if (this.getConfig().isSet("debug") == false) {
-			this.saveDefaultConfig();
-			log.info("[QuickSeeds] Config did not exist or was invalid, default config saved.");
-		}
-        
-        // Get plugin info from plugin.yml
-        PluginDescriptionFile pdfFile = this.getDescription();
-
+        // Add default values to config
+		this.saveDefaultConfig();
+		processDefaultConfig();
+		
         // Register events
-        getServer().getPluginManager().registerEvents(new QuickSeedsBlockListener(), this);
+        getServer().getPluginManager().registerEvents(new QuickSeedsBlockListener(this), this);
 
         // Output to console that plugin is enabled
-        log.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " enabled!");
+        log.info(this.getDescription().getName() + " version " + this.getDescription().getVersion() + " enabled!");
     }
-    
+
+	private void processDefaultConfig() {
+		final Map<String, Object> defParams = new HashMap<String, Object>();
+		this.getConfig().options().copyDefaults(true);
+
+		defParams.put("crops.wheat", true);
+		defParams.put("crops.carrot", true);
+		defParams.put("crops.potato", true);
+
+		for (final Entry<String, Object> e : defParams.entrySet())
+			if (!this.getConfig().contains(e.getKey()))
+				this.getConfig().set(e.getKey(), e.getValue());
+
+		this.saveConfig();
+		this.reloadConfig();
+	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
 		if (cmd.getName().equalsIgnoreCase("qsreload")) {
